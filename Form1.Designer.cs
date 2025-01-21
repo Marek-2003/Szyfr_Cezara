09fa135983df4eb2cfe1493cdcfe614e84889b13
marek <marek@DESKTOP-HB3MA4O>
2025-01-21 20:48:28 +0100

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace SzyfrCezara
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">True, jeśli wyczyścić zasoby zarządzane; w przeciwnym razie False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez projektanta
+
+        /// <summary>
+        /// Wymagana metoda do obsługi projektanta — nie edytuj
+        /// zawartości tej metody w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtShift = new System.Windows.Forms.TextBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.labelShift = new System.Windows.Forms.Label();
+            this.labelInput = new System.Windows.Forms.Label();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtShift
+            // 
+            this.txtShift.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.txtShift.Location = new System.Drawing.Point(143, 12);
+            this.txtShift.Name = "txtShift";
+            this.txtShift.Size = new System.Drawing.Size(40, 22);
+            this.txtShift.TabIndex = 0;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(136, 60);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(287, 76);
+            this.txtInput.TabIndex = 1;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(136, 158);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(287, 73);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.btnEncrypt.Location = new System.Drawing.Point(125, 250);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 3;
+            this.btnEncrypt.Text = "Szyfruj";
+            this.btnEncrypt.UseVisualStyleBackColor = false;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.btnDecrypt.Location = new System.Drawing.Point(358, 250);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 4;
+            this.btnDecrypt.Text = "Odszyfruj";
+            this.btnDecrypt.UseVisualStyleBackColor = false;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // labelShift
+            // 
+            this.labelShift.AutoSize = true;
+            this.labelShift.Location = new System.Drawing.Point(12, 15);
+            this.labelShift.Name = "labelShift";
+            this.labelShift.Size = new System.Drawing.Size(125, 16);
+            this.labelShift.TabIndex = 5;
+            this.labelShift.Text = "Podaj przesunięcie:";
+            // 
+            // labelInput
+            // 
+            this.labelInput.AutoSize = true;
+            this.labelInput.Location = new System.Drawing.Point(12, 41);
+            this.labelInput.Name = "labelInput";
+            this.labelInput.Size = new System.Drawing.Size(205, 16);
+            this.labelInput.TabIndex = 6;
+            this.labelInput.Text = "Wprowadź tekst (do szyfrowania):";
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Location = new System.Drawing.Point(12, 139);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(146, 16);
+            this.labelOutput.TabIndex = 7;
+            this.labelOutput.Text = "Wynik (po szyfrowaniu):";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(565, 326);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.labelInput);
+            this.Controls.Add(this.labelShift);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.txtShift);
+            this.Name = "Form1";
+            this.Text = "Szyfr Cezara";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtShift;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Label labelShift;
+        private System.Windows.Forms.Label labelInput;
+        private System.Windows.Forms.Label labelOutput;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SzyfrCezara
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Definicja pełnego alfabetu polskiego, który uwzględnia polskie znaki
+        private static readonly string Alphabet = "aąbcćdeęfghijklłmnńoóprsśtuwyzźż";
+
+        // Funkcja szyfrująca tekst za pomocą szyfru Cezara z obsługą polskich znaków
+        private string Encrypt(string input, int shift)
+        {
+            string result = "";
+            foreach (char c in input.ToLower()) // Przekształcamy tekst na małe litery
+            {
+                int index = Alphabet.IndexOf(c);
+                if (index >= 0)
+                {
+                    int newIndex = (index + shift) % Alphabet.Length;
+                    if (newIndex < 0) newIndex += Alphabet.Length; // Zapewnienie pozytywnego przesunięcia
+                    result += Alphabet[newIndex];
+                }
+                else
+                {
+                    result += c; // Inne znaki nie są zmieniane
+                }
+            }
+            return result;
+        }
+
+        // Funkcja deszyfrująca tekst za pomocą szyfru Cezara
+        private string Decrypt(string input, int shift)
+        {
+            return Encrypt(input, -shift); // Deszyfrowanie to szyfrowanie z odwrotnym przesunięciem
+        }
+
+        // Funkcja obsługująca kliknięcie przycisku szyfrowania
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtShift.Text, out int shift))
+            {
+                string input = txtInput.Text;
+                string encryptedText = Encrypt(input, shift);
+                txtOutput.Text = encryptedText;
+            }
+            else
+            {
+                MessageBox.Show("Proszę podać poprawne przesunięcie.");
+            }
+        }
+
+        // Funkcja obsługująca kliknięcie przycisku deszyfrowania
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtShift.Text, out int shift))
+            {
+                string input = txtInput.Text;
+                string decryptedText = Decrypt(input, shift);
+                txtOutput.Text = decryptedText;
+            }
+            else
+            {
+                MessageBox.Show("Proszę podać poprawne przesunięcie.");
+            }
+        }
+    }
+}
